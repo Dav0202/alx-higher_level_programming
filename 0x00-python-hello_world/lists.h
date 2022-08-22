4307ea9f465db9ec62dfed400c771725c3bea043
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-08-22 23:04:13 +0100

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,12 +2,10 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  */
 typedef struct listint_s
@@ -17,5 +15,4 @@ typedef struct listint_s
 } listint_t;
 
 int check_cycle(listint_t *list);
-
-#endif /* LISTS_H */
+#endif
