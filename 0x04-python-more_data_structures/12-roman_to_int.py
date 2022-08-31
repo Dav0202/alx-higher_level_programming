fc8fd73fc001d926548f51457955e6889ee60426
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-01 00:41:24 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
-def roman_to_int(roman_string):
-    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    i = 0
-    total = 0
-    if isinstance(roman_string, str):
-        for i in range(len(roman_string) - 1):
-            print('ih',i, roman_string[i])
-            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
-                total += num_map[roman_string[i]]
-            else:
-                total -= num_map[roman_string[i]]
-            i += 1
-        total += num_map[roman_string[i]]
-        return total
-    return 0
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
+
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i + 1]:
+            rep += numbers[i]
+        else:
+            rep -= numbers[i]
+
+    return rep
