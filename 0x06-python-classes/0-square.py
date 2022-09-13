fb57be3a678a9e412ad1cf71e050befa020b7f30
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-13 19:04:56 +0100

    Update 0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module defining class square
+"""
+class Square:
+    """ 
+     class square randomly generates integer to denote one side of a square and store it in the class
+    """
+    pass
