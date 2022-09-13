9923c2f747ac61d63ba0fa1c025bfd5f78d86874
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-13 22:14:50 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""
-Define Square class implements 
-checks attributes
-"""
+"""Define Square class implements checks attributes"""
+
+
 class Square:
      """Square class with a private attribute size."""
     def __init__(self, size=0):
@@ -11,8 +10,8 @@ class Square:
         Args:
             __size (int): The __size of the new square.
         """
+        self.__size = size
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
