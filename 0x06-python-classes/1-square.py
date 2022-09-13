2f954167f5a1c5134d674ebf5f40b6934ffae507
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-13 22:17:37 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,7 +8,10 @@ class Square:
     """
 Implementation
     """
-    
-    
     def __init__(self, size):
+        """Initializes the size variable as a private
+        instance artribute
+        Args:
+            __size (int): The __size of the new square.
+        """
         self.__size = size
