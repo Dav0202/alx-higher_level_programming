af31aba9d15427c10f4bc5a6d25aef5dc068cff2
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-20 15:24:02 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines `print_square`
+The function prints a square
+"""
 
+
+def print_square(size):
+    """prints a square with size, `size`
+    Args:
+        size (int)
+    Raises:
+        ValueError: size must be greater that or equal 0
+        TypeError: size must be an integer
+    """
+
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+
+    for x in range(size):
+        print('#' * size)
