3cb6aec4f9a51362dc4f84e2c6da9787b339cc3b
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-26 22:35:59 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
 
+""" Class that inherits from int """
+
+
+class MyInt(int):
+    """ Class int rebel == and != are inverted"""
+
+    def __eq__(self, other):
+        """ Call to Not Equal """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ Call to Equal """
+        return super().__eq__(other)
