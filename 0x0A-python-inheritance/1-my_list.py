603d56637a2356e6e78a0e7fd945fd74981dc09f
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-26 23:38:10 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""  Author David """
+"""
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
 
 
 class MyList(list):
     """ Class that inherits from list """
-    
     def print_sorted(self):
         """ Prints the list, but sorted """
         print(sorted(self))
