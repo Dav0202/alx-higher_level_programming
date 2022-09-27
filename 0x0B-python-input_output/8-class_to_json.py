2184c22796c07a310697ef0ab68ec1852d08bf53
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-27 23:24:37 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,10 +4,7 @@
 
 def class_to_json(obj):
     """ function that returns the dictionary
-    description with simple data
-    structure (list, dictionary, string, 
-    integer and boolean) for JSON
-    serialization of an object """
+    description with simple data """
     dic = {}
     if hasattr(obj, "__dict__"):
         dic = obj.__dict__.copy()
