18056c7b0dda516ab3e10d1b9e2d0f460a1407c0
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-27 15:49:14 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
 
+""" Student to json """
+
+
+class Student:
+    """ Class that defines a Student object """
+
+    def __init__(self, first_name, last_name, age):
+        """ Class Constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves Class dictionary base on attr
+        """
+        if not isinstance(attrs, list) or \
+           not all(isinstance(i, str) for i in attrs):
+
+            dic = self.__dict__.copy()
+        else:
+            dic = {i: self.__dict__[i] for i in attrs if i in self.__dict__}
+        return dic
