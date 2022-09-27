a3ed58694684fff9e973c935fd068cb27fc920e5
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-27 23:18:14 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Author David """"
+"""Write to a file"""
 
 
 def write_file(filename="", text=""):
-    """ function that writes a string to a text file (UTF8) and returns
-    the number of characters written """
+    """ function that writes a string to a text file (UTF8)
+    and returns the number of characters written """
     
     with open(filename, "w", encoding="utf-8") as myfile:
         return myfile.write(text)
