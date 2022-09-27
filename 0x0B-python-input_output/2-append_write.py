61ea5552d11f137b5f0477be63899476808ebbc2
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-27 15:01:06 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,10 @@
+#!/usr/bin/python3
+""" Author: David """
 
+
+def append_write(filename="", text=""):
+    """ function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added """
+
+    with open(filename, "a", encoding="utf-8") as myfile:
+        return myfile.write(text)
