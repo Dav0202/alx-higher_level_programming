1ae26e90e5732b1fa2efb8b1e462e71070d9d7fc
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-11-30 04:33:09 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < lang.length; i++) {
-  console.log(line[i]);
+for (const i of lang) {
+  console.log(i);
 }
